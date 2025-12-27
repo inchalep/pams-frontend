78d698427e76bcc0cb494b30bbfc8651a9fdbf23
idnyaneshwaraxioned <dnyaneshwari@axioned.com>
2025-12-27 18:53:20 +0530

    feat: update workflow

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import {variables} from '../constants/config.js';
-import store from '../store/index.js';
-
+const baseUrl = variables.BACKEND_URL;
+console.log({baseUrl})
 export const _post = (url, payload) => {
-  return axios.post(`${variables.BACKEND_URL}${url}`, payload, {
+  return axios.post(`${baseUrl}${url}`, payload, {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${localStorage?.getItem('token')}`,
@@ -12,7 +12,7 @@ export const _post = (url, payload) => {
 };
 
 export const _get = (url, options) => {
-  return axios.get(`${variables.BACKEND_URL}${url}`, {
+  return axios.get(`${baseUrl}${url}`, {
     ...options,
     headers: {
       'Content-Type': 'application/json',
@@ -22,7 +22,7 @@ export const _get = (url, options) => {
 };
 
 export const _patch = (url, payload) => {
-  return axios.patch(`${variables.BACKEND_URL}${url}`, payload, {
+  return axios.patch(`${baseUrl}${url}`, payload, {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${localStorage?.getItem('token')}`,
@@ -31,7 +31,7 @@ export const _patch = (url, payload) => {
 };
 
 export const _del = (url) => {
-  return axios.delete(`${variables.BACKEND_URL}${url}`, {
+  return axios.delete(`${baseUrl}${url}`, {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${localStorage?.getItem('token')}`,
